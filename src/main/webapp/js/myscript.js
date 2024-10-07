d7ceaeebae1312acea657b7ff9701134e20033bd
migyeong <wjdalrud5516@gmail.com>
2024-10-08 00:58:35 +0900

    javascript - bbs,member 테이블 유효성 검사

--- src/main/webapp/js/myscript.js
+++ src/main/webapp/js/myscript.js
@@ -100,3 +100,110 @@ function loginCheck(){
 
 	return true;
 }//loginCheck() end
+
+//회원가입 시 아이디 중복체크 여부
+function idCheck(){
+	//모달창
+	//->부모창과 자식창이 한몸으로 구성되어 있음
+	//->참조 https://www.w3schools.com/bootstrap/bootstrap_modal.asp
+	
+	//새창 만들기(팝업)
+	//->부모창과 자식창이 별개로 구성되어 있음
+	//->모바일에 기반을 둔 frontend단(SPA)에서는 사용하지 말것!!
+	//->참조 https://www.w3schools.com/jsref/met_win_open.asp
+	//window.open("파일명", "새창이름", "다양한옵션들")
+	window.open("idCheckForm.jsp", "idwin", "width=400, height=350");
+}//idCheck() end
+
+//과제) 회원가입 시 이메일 중복체크 여부
+function emailCheck(){
+	window.open("emailCheckForm.jsp", "emailwin", "width=400, height=350");
+}//emailCheck() end
+
+//회원가입 유효성 검사
+function memberCheck(){
+	//1)아이디 5~10글자 인지?
+	let id = $("#id").val();
+	id = id.trim();
+	if(!(id.length >= 5 && id.length <= 10)){
+		alert("아이디를 5~10글자 이내로 입력해주세요");
+		$("#id").focus();
+		return false;
+	}//if end
+
+	//아이디명 금칙어 : guest
+	//auth.jsp에서 로그인하지 않은 id는 "guest"로 저장
+	if(id == "guest"){
+		alert("guest는 사용 불가능한 ID입니다");
+		return false;
+	}//if end
+	
+    //2)비밀번호 5~10글자 인지?
+	let passwd = $("#passwd").val();
+	passwd = passwd.trim();
+	if(!(passwd.length >= 5 && passwd.length <= 10)){
+		alert("비밀번호를 5~10글자 이내로 입력해주세요");
+		$("#passwd").focus();
+		return false;
+	}//if end
+	
+    //3)비밀번호와 비밀번호확인이 서로 일치하는지?
+	let repasswd = $("#repasswd").val();
+	repasswd = repasswd.trim();
+	if(passwd.length < 5 || passwd != repasswd){
+		alert("비밀번호를 다시 한번 확인해주세요");
+		$("#repasswd").focus();
+		return false;
+	}//if end
+
+    //4)이름 두글자 이상 인지?
+	let mname = $("#mname").val();
+	mname = mname.trim();
+	if(mname.length < 2){
+		alert("이름을 2글자 이상 입력해주세요");
+		$("#mname").focus();
+		return false;
+	}//if end
+
+    //5)이메일 5글자 인지?
+	let email = $("#email").val();
+	email = email.trim();
+	if(email.length < 5){
+		alert("이메일을 다시 한번 확인해주세요");
+		$("#email").focus();
+		return false;
+	}//if end
+
+    //6)직업을 선택했는지?
+	let job = $("#job").val();
+	if(job == "0"){
+		alert("직업을 선택해주세요");
+		return false;
+	}//if end
+
+	return true; //위 모든 조건 충족 시 submit
+}//memberCheck() end
+
+//아이디/비밀번호 찾기 유효성 검사
+function findIDCheck(){
+	//1) 이름 2글자 이상인지 확인
+	let mname = $("#mname").val();
+	mname = mname.trim();
+	if(mname.length < 2){
+		alert("이름을 2글자 이상 입력해주세요");
+		$("#mname").focus();
+		return false;
+	}//if end
+	
+	//2)이메일 5글자 인지?
+	let email = $("#email").val();
+	email = email.trim();
+	const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+				
+	if(pattern.test(email) === false){
+		alert("이메일 형식에 맞지 않습니다");
+		return false;
+	}//if end
+
+	return true;
+}//loginCheck() end
